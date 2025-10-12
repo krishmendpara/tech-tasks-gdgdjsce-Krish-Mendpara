d94dfad30a3f216a43cdc2734d9fb7123e018d83
krishmendpara <krishmendpara5@gmail.com>
2025-10-12 10:11:53 +0530

    Added Hero Section

---
+++ Task-1-Figma-Blueprint/Task1/src/components/HeroSection.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="flex flex-col-reverse md:flex-row items-center justify-between bg-gradient-to-r from-[#5C3DFF] to-[#120e6a] px-4 md:px-12 py-10 rounded-4xl">
+      
+      <div className="w-full md:w-1/2 max-w-xl text-white md:pr-10">
+        <h1 className="text-4xl md:text-5xl font-extrabold mb-6 leading-tight text-left">
+          Experience the road like never before
+        </h1>
+        <p className="mb-8 text-lg md:text-xl font-medium opacity-85 text-left">
+          Aliquam adipiscing velit semper morbi. Purus non eu cursus porttitor tristique et gravida. Quis nunc interdum gravida ullamcorper.
+        </p>
+        <button className="bg-[#FF9E0C] hover:hover:bg-[#ffc369]  transition font-semibold flex justify-between rounded-lg px-6 py-3  text-white">
+          View all cars
+        </button>
+      </div>
+
+     
+      <div className="w-full md:w-[370px] mb-10 md:mb-0">
+        <div className="bg-white rounded-2xl shadow-lg p-8 w-full">
+          <h2 className="font-bold text-2xl mb-6 text-gray-900">Book your car</h2>
+          <form className="flex flex-col gap-4">
+            <select className="p-3 rounded-lg border border-gray-200 focus:outline-none">
+              <option>Car type</option>
+            </select>
+            <select className="p-3 rounded-lg border border-gray-200 focus:outline-none">
+              <option>Place of rental</option>
+            </select>
+            <select className="p-3 rounded-lg border border-gray-200 focus:outline-none">
+              <option>Place of return</option>
+            </select>
+            <input type="date" className="p-3 rounded-lg border border-gray-200 focus:outline-none" placeholder="Rental date" />
+            <input type="date" className="p-3 rounded-lg border border-gray-200 focus:outline-none" placeholder="Return date" />
+            <button type="submit" className="mt-2 bg-[#FF9E0C]  hover:bg-[#ffc369] transition w-full rounded-lg text-white font-semibold py-3">
+              Book now
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- Task-1-Figma-Blueprint/Task1/src/components/Navbar.jsx
+++ Task-1-Figma-Blueprint/Task1/src/components/Navbar.jsx
@@ -15,16 +15,16 @@ const Navbar = () => {
 
 
                 <div className="hidden md:flex space-x-6">
-                    <a href="#" className="text-black hover:text-purple-600 font-medium">Home</a>
-                    <a href="#" className="text-black hover:text-purple-600 font-medium">Vehicles</a>
-                    <a href="#" className="text-black hover:text-purple-600 font-medium">Details</a>
-                    <a href="#" className="text-black hover:text-purple-600 font-medium">About Us</a>
-                    <a href="#" className="text-black hover:text-purple-600 font-medium">Contact Us</a>
+                    <a href="#" className="text-black hover:text-[#5937E0] font-medium">Home</a>
+                    <a href="#" className="text-black hover:text-[#5937E0] font-medium">Vehicles</a>
+                    <a href="#" className="text-black hover:text-[#5937E0] font-medium">Details</a>
+                    <a href="#" className="text-black hover:text-[#5937E0] font-medium">About Us</a>
+                    <a href="#" className="text-black hover:text-[#5937E0] font-medium">Contact Us</a>
                 </div>
 
 
                 <div className="flex items-center space-x-2 px-3 py-1 rounded-full ">
-                  <div className="bg-purple-600 rounded-full w-9 h-9 flex items-center justify-center mr-0">
+                  <div className="bg-[#5937E0] rounded-full w-9 h-9 flex items-center justify-center mr-0">
                     <Phone className="w-4 h-4 text-white " />
                     </div>  
 
